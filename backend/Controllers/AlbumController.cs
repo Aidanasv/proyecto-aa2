b6c05fedf7d25b2865cbe2850c9b30ec42004171
Aidanasv <a27818@svalero.com>
2025-09-02 01:41:58 +0200

    Refactor AlbumController y ArtistController para mejorar la lectura de datos. Se implementa la proyección a DTOs en los métodos GetAlbums, GetAlbumById, CreateAlbum, UpdateAlbum, GetArtists, GetArtistById, CreateArtist y UpdateArtist. Se añaden mensajes de error más descriptivos en las respuestas.

--- backend/Controllers/AlbumController.cs
+++ backend/Controllers/AlbumController.cs
@@ -17,24 +17,50 @@ public class AlbumController : ControllerBase
         _albumService = service;
     }
 
+    //Obtener Albumes
     [HttpGet]
     public async Task<ActionResult<List<AlbumRead>>> GetAlbums()
     {
         var albums = await _albumService.GetAllAsync();
-        return Ok(albums);
+        var albumsRead = albums.Select(album => new AlbumRead
+        {
+            Name = album.Name,
+            ArtistId = album.ArtistId,
+            ReleaseDate = album.ReleaseDate,
+            Duration = album.Duration,
+            Imagen = album.Imagen,
+            SoftDelete = album.SoftDelete,
+            Id = album.Id
+        });
+
+        return Ok(albumsRead);
     }
 
+    //Obtener album por Id
     [HttpGet("{id}")]
     public async Task<ActionResult<AlbumRead>> GetAlbumById(int id)
     {
         var album = await _albumService.GetByIdAsync(id);
         if (album == null)
         {
-            return NoContent();
+            return NotFound(new { message = "No se encontró el albúm" });
+
         }
-        return Ok(album);
+
+        var albumRead = new AlbumRead
+        {
+            Name = album.Name,
+            ArtistId = album.ArtistId,
+            ReleaseDate = album.ReleaseDate,
+            Duration = album.Duration,
+            Imagen = album.Imagen,
+            SoftDelete = album.SoftDelete,
+            Id = album.Id
+        };
+        return Ok(albumRead);
     }
 
+    //Crear album - Admin
     [Authorize(Roles = Role.Admin)]
     [HttpPost]
     public async Task<ActionResult<AlbumRead>> CreateAlbum(AlbumCreate album)
@@ -46,18 +72,29 @@ public class AlbumController : ControllerBase
                 return BadRequest(ModelState);
             }
             var newAlbum = await _albumService.AddAsync(album);
+            var albumRead = new AlbumRead
+            {
+                Name = newAlbum.Name,
+                ArtistId = newAlbum.ArtistId,
+                ReleaseDate = newAlbum.ReleaseDate,
+                Duration = newAlbum.Duration,
+                Imagen = newAlbum.Imagen,
+                SoftDelete = newAlbum.SoftDelete,
+                Id = newAlbum.Id
+            };
             return CreatedAtAction(
                 nameof(GetAlbumById),
-                new { id = newAlbum.Id },
-                newAlbum
+                new { id = albumRead.Id },
+                albumRead
             );
         }
         catch (Exception ex)
         {
-            return BadRequest(ex.Message);
+            return BadRequest(new { message = "Error al crear album" });
         }
     }
 
+    //Modificar album - Admin
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateAlbum(int id, AlbumCreate updatedAlbum)
     {
@@ -68,14 +105,26 @@ public class AlbumController : ControllerBase
                 return BadRequest(ModelState);
             }
             updatedAlbum = await _albumService.UpdateAsync(updatedAlbum, id);
-            return Ok(updatedAlbum);
+
+            var albumRead = new AlbumRead
+            {
+                Name = updatedAlbum.Name,
+                ArtistId = updatedAlbum.ArtistId,
+                ReleaseDate = updatedAlbum.ReleaseDate,
+                Duration = updatedAlbum.Duration,
+                Imagen = updatedAlbum.Imagen,
+                SoftDelete = updatedAlbum.SoftDelete,
+                Id = id
+            };
+            return Ok(albumRead);
         }
-        catch (Exception ex)
+        catch
         {
-            return BadRequest(ex.Message);
+            return BadRequest(new { message = "Error al modificar album" });
         }
     }
 
+    //Eliminar album - admin
     [Authorize(Roles = Role.Admin)]
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteAlbum(int id)
@@ -83,19 +132,20 @@ public class AlbumController : ControllerBase
         var album = await _albumService.GetByIdAsync(id);
         if (album == null)
         {
-            return NotFound();
+            return NotFound(new { message = "Error al eliminar album" });
         }
         await _albumService.DeleteAsync(id);
-        return Ok();
+        return Ok(id);
     }
 
+    //Obtener tracks de album
     [HttpGet("{id}/tracks")]
     public async Task<ActionResult<AlbumTrackDTO>> GetTracksByAlbum(int id)
     {
         var albums = await _albumService.GetTracksByAlbum(id);
         if (albums == null)
         {
-            return NoContent();
+            return NotFound(new { message = "No se encontraron tracks para el album" });
         }
         return Ok(albums);
     }
